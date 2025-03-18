0b2de6a186882b30f112dc6fe9c66e0af91448bd
David Spörri <davepermen@hotmail.com>
2025-03-18 16:59:47 +0100

    improvements

--- src/Features/RssFinder/Services/RssFinderService.cs
+++ src/Features/RssFinder/Services/RssFinderService.cs
@@ -1,49 +1,68 @@
 ﻿using AngleSharp.Html.Parser;
 using Conesoft.Tools;
 using System.Buffers;
+using System.Runtime.CompilerServices;
 
 namespace Conesoft_Website_News.Features.RssFinder.Services;
 
 public class RssFinderService(IHttpClientFactory factory)
 {
-    readonly SearchValues<string> searchValues = SearchValues.Create(["<html", "<rss"], StringComparison.OrdinalIgnoreCase);
-
-    public async Task<string[]> FindRssFeedsOnUrl(string url, bool trymore = true)
+    public async IAsyncEnumerable<string> FindRssFeedsOnUrl(string url, [EnumeratorCancellation] CancellationToken cancellation)
     {
-        url = url.StartsWith("https://") ? url : $"https://{url}";
+        url = FixUrl(url);
+
+        Queue<string> links = new([url, url + "/rss", url + "/rss/", url + "/feed", url + "/feed/", url + "/rss.xml", url + "/feed.xml"]);
+        HashSet<string> visited = [];
 
         var client = factory.CreateClient();
-        ReadOnlySpan<char> content = (await Safe.TryAsync(() => client.GetStringAsync(url))) ?? "";
 
-        var found = content.IndexOfAny(searchValues) switch
+        while (links.Count > 0 && cancellation.IsCancellationRequested == false)
         {
-            -1 => [],
-            int i when content[i..].StartsWith("<html") => await FindInHtmlDocument(content.ToString(), url),
-            int i when content[i..].StartsWith("<rss") => [url],
-            _ => []
-        };
+            var current = links.Dequeue();
+            visited.Add(current);
 
-        if (trymore == true)
-        {
-            url = url.EndsWith('/') ? url[..^1] : url;
-            found = found.Length > 0 ? found : await FindRssFeedsOnUrl(url + "/feed/", trymore = false);
-            found = found.Length > 0 ? found : await FindRssFeedsOnUrl(url + "/rss.xml", trymore = false);
+            var content = (await Safe.TryAsync(() => client.GetStringAsync(current, cancellation))) ?? "";
+
+            if (IsRssFeed(content))
+            {
+                yield return current;
+            }
+            else
+            {
+                foreach (var link in (await LinksInHtml(content, current)).Where(link => visited.Contains(link) == false))
+                {
+                    links.Enqueue(link);
+                }
+            }
+
         }
-
-        return found;
     }
 
-    private static async Task<string[]> FindInHtmlDocument(string content, string url)
+    readonly static SearchValues<string> rssSearchValues = SearchValues.Create(["<rss", "<feed", "<?xml"], StringComparison.OrdinalIgnoreCase);
+    static bool IsRssFeed(ReadOnlySpan<char> content) => content.ContainsAny(rssSearchValues);
+
+    static async Task<string[]> LinksInHtml(string content, string url)
     {
         var document = await new HtmlParser().ParseDocumentAsync(content);
         return [.. document
-            .QuerySelectorAll("[type='application/rss+xml']")
-            .Select(tag => Safe.Try(() => FixIfRelativeUrl(tag.GetAttribute("href"), url)))
+            .QuerySelectorAll("link[href][type='application/rss+xml'],a[href*='/rss']")
+            .Select(tag => FixIfRelativeUrl(tag.GetAttribute("href"), url))
             .NotNull()
+            .Where(link => IsSameHost(link, url))
+            .DistinctBy(url => new Uri(url).AbsolutePath)
         ];
     }
 
-    private static string? FixIfRelativeUrl(string? url, string domain)
+    static bool IsSameHost(string uri1, string uri2) => new Uri(uri1, UriKind.Absolute).Host.Replace("www.", "") == new Uri(uri2, UriKind.Absolute).Host.Replace("www.", "");
+
+    static string FixUrl(string url)
+    {
+        url = url.StartsWith("https://") ? url : $"https://{url}";
+        url = url.EndsWith('/') ? url[..^1] : url;
+        return url;
+    }
+
+    static string? FixIfRelativeUrl(string? url, string domain)
     {
         if (url == null) return null;
 
